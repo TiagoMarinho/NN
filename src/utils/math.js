4c52a6115ec7da7e9b9e97d3b7998c64d0dad6ec
Tiago Marinho <tiagomeraki@gmail.com>
2026-03-10 16:32:13 +0100

    separate neural net utilities

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,7 +1,6 @@
 export const getRandomFloat = (minimum, maximum) => Math.random() * (maximum - minimum) + minimum;
 export const getRandomInt = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 export const getSum = (numbers) => numbers.reduce((total, n) => total + n, 0);
-export const getHeWeightScale = (inputSize) => Math.sqrt(2 / inputSize);
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 export const lerp = (start, end, interpolation) => start + (end - start) * interpolation;
 export const round = (value, decimals = 2) => {
---
+++ src/utils/nn.js
@@ -0,0 +1,2 @@
+export const getHeWeightScale = (inputSize) => Math.sqrt(2 / inputSize);
+export const getXavierScale = (inputSize) => Math.sqrt(1 / inputSize);
