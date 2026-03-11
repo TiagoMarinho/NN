31d876d0312bf4a12a94d276929c41db1f08bea7
Tiago Marinho <tiagomeraki@gmail.com>
2026-03-11 18:04:56 +0100

    specify loss and activation functions

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import { NeuralNetwork, schedulers } from "./neural_network.js";
+import { NeuralNetwork, schedulers, losses, activations } from "./neural_network.js";
 import { TASKS } from "./tasks.js";
 import { train, evaluate } from "./train.js";
 
-const BITS = 8;
-
+const BITS = 16;
+const BATCH_SIZE = 4
 const CONFIG = {
 	BITS,
 	TASK: TASKS.parity(BITS),
-	HIDDEN_LAYERS: [32, 16, 16],
-	TOTAL_EPOCHS: 500,
+	HIDDEN_LAYERS: [64, 64],
+	TOTAL_EPOCHS: 1000,
 	SAMPLES_PER_EPOCH: 1000,
-	BATCH_SIZE: 1,
+	BATCH_SIZE,
 	LOG_FREQUENCY: 100,
-	SCHEDULER: (e, t) => schedulers.cosine(e, t, 0.01, 0.001),
+	SCHEDULER: (e, t) => schedulers.linear(e, t, 0.05, 0.002),
+	LOSS: losses.mse,
+	ACTIVATION: activations.sigmoid,
 };
 
-const network = new NeuralNetwork(CONFIG.BITS, CONFIG.HIDDEN_LAYERS, CONFIG.TASK.outputSize);
+const network = new NeuralNetwork(CONFIG.BITS, CONFIG.HIDDEN_LAYERS, CONFIG.TASK.outputSize, CONFIG.ACTIVATION);
 
 train(network, CONFIG);
 evaluate(network, CONFIG);
